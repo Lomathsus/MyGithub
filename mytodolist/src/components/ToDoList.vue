ffe132bb667f802cf47fca2c39410f7f0172189a
XiangyuCao <xiangyu1205@gmail.com>
2022-05-19 15:31:14 +0800

    My todolist practice

---
+++ mytodolist/src/components/ToDoList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <input v-model="inputValue" />
+        <button @click="clickEvent">提交</button>
+        <ul>
+            <listitem v-for="(item, index) in list" :key="index" :content="item" :index="index" @delete="deleteEvent"></listitem>
+        </ul>
+    </div>
+</template>
+
+<script>
+import listitem from './listitem.vue'
+
+export default ({
+    name: 'ToDoList',
+    components: {
+        listitem
+    },
+    data() {
+        return {
+            inputValue: '',
+            list: []
+        }
+    },
+    methods: {
+        clickEvent: function() {
+            this.list.push(this.inputValue);
+            this.inputValue = '';
+        },
+
+        deleteEvent: function(x) {
+            this.list.splice(x,1);
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
---
+++ mytodolist/src/components/listitem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li @click="clickEvent">{{content}}</li>
+</template>
+
+<script>
+export default ({
+    name: 'listitem',
+    props: [
+        'content',
+        'index'
+    ],
+    methods: {
+        clickEvent: function() {
+            this.$emit('delete',this.index);
+        }
+    },
+})
+</script>
